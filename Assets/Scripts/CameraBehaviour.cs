200e7d209a79b7086233750e92c3e8d887bfe168
Ryan Lee <ryan@sparcs.org>
2025-01-09 20:46:07 +0900

    make prefab

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -5,7 +5,8 @@ public class CameraBehaviour : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = new Vector3(30, 30, 30); 
+        transform.LookAt(Vector3.zero);
     }
 
     // Update is called once per frame
