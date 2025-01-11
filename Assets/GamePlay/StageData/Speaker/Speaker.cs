ee44ae890fa9931a45eb87ac0a9438cfda3ef65f
Ryan Lee <ryan@sparcs.org>
2025-01-11 14:22:48 +0900

    speaker particle system

--- Assets/GamePlay/StageData/Speaker/Speaker.cs
+++ Assets/GamePlay/StageData/Speaker/Speaker.cs
@@ -1,23 +1,35 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace GamePlay.StageData.Speaker
 {
     public class Speaker : StageElementBehaviour
     {
-        public GameObject particleSystem;
+        public bool IsSoundOn { get; set; }
+        private ParticleSystem _soundParticleSystem;
+        private const float MaxLifetime = 75f;
         
         public override void OnClicked() { }
 
         public override void Start()
         {
             base.Start();
+            _soundParticleSystem = GetComponentInChildren<ParticleSystem>();
             TurnOnSound();
         }
 
         public void TurnOnSound()
         {
-            particleSystem.SetActive(true);
+            IsSoundOn = true;
+            _soundParticleSystem.Simulate(MaxLifetime);
+            _soundParticleSystem.Play();
+        }
+        
+        public void TurnOffSound()
+        {
+            IsSoundOn = false;
+            _soundParticleSystem.Stop();
         }
     }
 }
