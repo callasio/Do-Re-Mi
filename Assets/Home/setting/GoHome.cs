231521756a945555188d4782e0a4abd1d4906ed3
aldls <jealswl04@gmail.com>
2025-01-14 18:32:54 +0900

    null error

--- Assets/Home/setting/GoHome.cs
+++ Assets/Home/setting/GoHome.cs
@@ -2,35 +2,38 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GoHome : MonoBehaviour
+namespace Home.setting
 {
-    // Image 컴포넌트를 참조합니다.
-    public Image targetImage;
-    private Button _button;
+    public class GoHome : MonoBehaviour
+    {
+        // Image 컴포넌트를 참조합니다.
+        public Image targetImage;
+        private Button _button;
     
-    private void Start()
-    {
-        _button = GetComponentInChildren<Button>();
-        _button.onClick.AddListener(OnImageClick);
-        // _button.onClick.Invoke();
-        Debug.Log("Image clicked");
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.H))
+        private void Start()
         {
-            OnImageClick();
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(OnImageClick);
+            // _button.onClick.Invoke();
+            Debug.Log("Image clicked");
         }
-    }
 
-    // 클릭 시 실행되는 함수
-    private void OnImageClick()
-    {
-        // 다음 씬 이름을 설정하세요.
-        string nextSceneName = "HomeScene";
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.H))
+            {
+                OnImageClick();
+            }
+        }
+
+        // 클릭 시 실행되는 함수
+        private void OnImageClick()
+        {
+            // 다음 씬 이름을 설정하세요.
+            string nextSceneName = "HomeScene";
         
-        // 씬을 로드합니다.
-        SceneManager.LoadScene(nextSceneName);
+            // 씬을 로드합니다.
+            SceneManager.LoadScene(nextSceneName);
+        }
     }
 }
